8c5657f4c3a67dda79f989c70effbcdc32cdc61a
CassiusClose@gmail.com <CassiusClose@gmail.com>
2021-01-27 20:14:34 -0500

    Setup project with MVVM setup, including base classes

---
+++ PhotoGalleryApp/ViewModels/RelayCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PhotoGalleryApp.ViewModels
+{
+    class RelayCommand : ICommand
+    {
+        // The function that executes the command
+        private readonly Action<object> _execute;
+
+        // The function that determines whether or not the command can be executed
+        private readonly Func<object, bool> _canExecuteAction;
+
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecuteAction)
+        {
+            _execute = execute;
+            _canExecuteAction = canExecuteAction;
+        }
+
+        public RelayCommand(Action<object> execute) : this(execute, null) { }
+
+
+        /*
+         * Execute the command
+         */
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        /*
+         * Returns whether or not the command can be executed
+         */
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecuteAction != null)
+                return _canExecuteAction(parameter);
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        /*
+         * Tell the ICommand interface that the conditions that affect whether or not
+         * the command can execute have changed. Any classes that use this class should
+         * call this when changing those conditions.
+         */
+        public void InvokeCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ PhotoGalleryApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoGalleryApp.ViewModels
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /*
+         * Notifies listeners that a property in the class has changed. Subclasses should
+         * call this on a property whenever changing it.
+         * 
+         * [CallerMemberName] automatically provides the propertyName argument when called
+         * from inside a property's setter. If called outside that property's getters & setters,
+         * will have to explicitly pass the property's name.
+         * 
+         * Implementation from
+         * https://docs.microsoft.com/en-us/archive/msdn-magazine/2009/february/patterns-wpf-apps-with-the-model-view-viewmodel-design-pattern
+         * and
+         * https://intellitect.com/getting-started-model-view-viewmodel-mvvm-pattern-using-windows-presentation-framework-wpf/
+         */
+        protected bool OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+            return false;
+        }
+    }
+}
