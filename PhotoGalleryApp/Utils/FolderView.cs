13342681f9038abd53d488440413b52ef7051764
Cassius Close <CassiusClose@gmail.com>
2023-07-24 23:54:09 -0400

    Moved folder maintainance to FolderView class
    Created a FolderView class which is responsible for maintaining the folder list displaying each year, month, and event in a MediaCollection.

---
+++ PhotoGalleryApp/Utils/FolderView.cs
@@ -0,0 +1,297 @@
+﻿using PhotoGalleryApp.Models;
+using PhotoGalleryApp.ViewModels;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoGalleryApp.Utils
+{
+    /// <summary>
+    /// Maintains a list of folders for each year, month, and event of a MediaCollection.
+    /// </summary>
+    public class FolderView
+    {
+        public FolderView(MediaCollection coll)
+        {
+            _collection = coll;
+            _collection.CollectionChanged += MediaCollectionChanged;
+
+            Folders = new ObservableCollection<FolderLabelViewModel>();
+            ResetList();
+        }
+
+        private MediaCollection _collection;
+
+        public ObservableCollection<FolderLabelViewModel> Folders
+        {
+            get;
+        }
+
+
+
+        public FolderLabelViewModel.FolderOpenedDelegate FolderOpened;
+
+        /** Pass child folder opened events along to listeners */
+        private void ChildFolderOpened(FolderLabelViewModel vm)
+        {
+            if(FolderOpened != null)
+                FolderOpened(vm);
+        }
+
+
+
+        /** Update the list of folders if events removed or added */
+        private void MediaCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch(e.Action)
+            {
+                case NotifyCollectionChangedAction.Add:
+                    if (e.NewItems == null)
+                        throw new ArgumentException("Adding items to MediaCollection, but NewItems is null");
+
+                    MediaCollectionChanged_Add(e.NewItems);
+                    break;
+
+                case NotifyCollectionChangedAction.Remove:
+                    if (e.OldItems == null)
+                        throw new ArgumentException("Removing items from MediaCollection, but OldItems is null");
+
+                    MediaCollectionChanged_Remove(e.OldItems);
+                    break;
+
+                case NotifyCollectionChangedAction.Replace:
+                    if (e.NewItems == null || e.OldItems == null)
+                        throw new ArgumentException("Replacing items in MediaCollection, but NewItems or OldItems is null");
+
+                    MediaCollectionChanged_Add(e.NewItems);
+                    MediaCollectionChanged_Remove(e.OldItems);
+                    break;
+
+                case NotifyCollectionChangedAction.Reset:
+                    ResetList();
+                    break;
+            }
+        }
+
+        private void MediaCollectionChanged_Add(IList newItems)
+        {
+            foreach(ICollectable c in newItems)
+            {
+                if (c is Media)
+                    continue;
+
+                // For each new event, add an entry into its proper folder
+                Event e = (Event)c;
+                FolderLabelViewModel month = GetMonthFolder(e.Collection.StartTimestamp);
+                FolderLabelViewModel eventFolder = CreateEventFolder(e);
+                eventFolder.FolderOpened += ChildFolderOpened;
+                month.Children.Add(eventFolder);
+            }
+        }
+
+        private void MediaCollectionChanged_Remove(IList oldItems)
+        {
+            foreach(ICollectable c in oldItems)
+            {
+                PrecisionDateTime ts;
+                if (c is Media)
+                    ts = ((Media)c).Timestamp;
+                else
+                    ts = ((Event)c).Collection.StartTimestamp;
+
+                // Find the year and month folder of the event
+
+                int yearInd = GetYearIndex(ts);
+                if (yearInd == -1)
+                    throw new Exception("Year folder doesn't exist when removing item");
+                FolderLabelViewModel year = Folders[yearInd];
+
+                int monthInd = GetMonthIndex(year, ts);
+                if (monthInd == -1)
+                    throw new Exception("Month folder doesn't exist when removing item");
+                FolderLabelViewModel month = year.Children[monthInd];
+
+
+
+                if (c is Event)
+                {
+                    Event e = (Event)c;
+
+                    // Find the event, and remove it
+                    for (int i = 0; i < month.Children.Count; i++)
+                    {
+                        if (month.Children[i].Label == e.Name)
+                        {
+                            month.Children.RemoveAt(i);
+                            break;
+                        }
+                    }
+                }
+
+                // Find if media exists for the year and month
+                bool monthFound = false;
+                bool yearFound = false;
+                PrecisionDateTime pdt;
+                foreach(ICollectable co in _collection)
+                {
+                    if (co is Media)
+                        pdt = ((Media)co).Timestamp;
+                    else
+                        pdt = ((Event)co).Collection.StartTimestamp;
+
+                    if (pdt.Year == ts.Year)
+                        yearFound = true;
+
+                    if(pdt.Month == ts.Month)
+                    {
+                        monthFound = true;
+                        break;
+                    }
+                }
+
+                // If the month or year folders are now empty, remove them
+                if (!monthFound)
+                    year.Children.RemoveAt(monthInd);
+
+                if (!yearFound)
+                    Folders.RemoveAt(yearInd);
+            }
+        }
+
+        /**
+         * Rebuilds the list of folders. There is a folder for each year and month that contains 
+         * media, and a folder for each event in its month folder.
+         */
+        private void ResetList()
+
+        {
+            Folders.Clear();
+            foreach(ICollectable vm in _collection)
+            {
+                if(vm is Media)
+                {
+                    Media m = (Media)vm;
+                    // Create folder if doesn't exist
+                    FolderLabelViewModel month = GetMonthFolder(m.Timestamp);
+                }
+                else
+                {
+                    Event e = (Event)vm;
+                    // Create folder if doesn't exist
+                    FolderLabelViewModel month = GetMonthFolder(e.Collection.StartTimestamp);
+                    month.Children.Add(CreateEventFolder(e));
+                }
+            }
+        }
+
+
+
+
+        /** Create a folder for an event, and nest folders for each child event in it */
+        private FolderLabelViewModel CreateEventFolder(Event e)
+        {
+            FolderLabelViewModel vm = new FolderLabelViewModel(e.Name, new PrecisionDateTime(e.Collection.StartTimestamp, TimeRange.Second));
+            vm.FolderOpened += ChildFolderOpened;
+            foreach(ICollectable c in e.Collection)
+            {
+                if (c is Event)
+                    vm.Children.Add(CreateEventFolder((Event)c));
+            }
+            return vm;
+        }
+
+
+
+
+        /** Gets the index of the given month's folder in the year folder */
+        private int GetMonthIndex(FolderLabelViewModel year, PrecisionDateTime ts)
+        {
+            PrecisionDateTime goal = new PrecisionDateTime(ts, TimeRange.Month);
+
+            int i = 0;
+            for(i = 0; i < year.Children.Count; i++)
+            {
+                if (year.Children[i].Timestamp == goal)
+                    return i;
+                else if (year.Children[i].Timestamp > goal)
+                    break;
+            }
+
+            return -1;
+        }
+
+        /** 
+         * Returns the month folder given by the timestamp. Will create the year or month folder
+         * if they do not already exist
+         */
+        private FolderLabelViewModel GetMonthFolder(PrecisionDateTime ts)
+        {
+            FolderLabelViewModel year = GetYearFolder(ts);
+
+            PrecisionDateTime goal = new PrecisionDateTime(ts, TimeRange.Month);
+
+            int i = 0;
+            for(i = 0; i < year.Children.Count; i++)
+            {
+                if (year.Children[i].Timestamp == goal)
+                    return year.Children[i];
+                else if (year.Children[i].Timestamp > goal)
+                    break;
+            }
+
+            FolderLabelViewModel v = new FolderLabelViewModel(ts.ToString("MMMM"), goal);
+            v.FolderOpened += ChildFolderOpened;
+            year.Children.Insert(i,v);
+            return v;
+        }
+
+        /** Gets the index of the given year's folder in list of folders */
+        private int GetYearIndex(PrecisionDateTime ts)
+        {
+            PrecisionDateTime goal = new PrecisionDateTime(ts, TimeRange.Year);
+
+            int i = 0;
+            for(i = 0; i < Folders.Count; i++)
+            {
+                if (Folders[i].Timestamp == goal)
+                    return i;
+                else if (Folders[i].Timestamp > goal)
+                    break;
+            }
+
+            return -1;
+        }
+
+        /** 
+         * Returns the year folder given by the timestamp. Will create the year folder
+         * if it does not already exist
+         */
+        private FolderLabelViewModel GetYearFolder(PrecisionDateTime ts)
+        {
+            PrecisionDateTime goal = new PrecisionDateTime(ts, TimeRange.Year);
+
+            int i = 0;
+            for(i = 0; i < Folders.Count; i++)
+            {
+                if (Folders[i].Timestamp == goal)
+                    return Folders[i];
+                else if (Folders[i].Timestamp > goal)
+                    break;
+            }
+            FolderLabelViewModel v = new FolderLabelViewModel(ts.Year.ToString(), goal);
+            v.FolderOpened += ChildFolderOpened;
+            Folders.Insert(i,v);
+            return v;
+        }
+
+
+
+
+
+    }
+}
--- PhotoGalleryApp/ViewModels/EventsViewModel.cs
+++ PhotoGalleryApp/ViewModels/EventsViewModel.cs
@@ -29,267 +29,22 @@ namespace PhotoGalleryApp.ViewModels
 
             _nav = nav; 
             _collection = coll;
-            _collection.CollectionChanged += MediaCollectionChanged;
 
-            _labels = new ObservableCollection<FolderLabelViewModel>();
-
-            ResetList();
+            _folders = new FolderView(coll);
+            _folders.FolderOpened += FolderOpened;
         }
 
 
         private NavigatorViewModel _nav;
 
         private MediaCollection _collection;
+        private FolderView _folders;
 
-        private ObservableCollection<FolderLabelViewModel> _labels;
         /// <summary>
         /// A list of folders, one for each year that contains media
         /// </summary>
-        public ObservableCollection<FolderLabelViewModel> Labels
-        { get { return _labels; } }
-
-
-        /** Update the list of folders if events removed or added */
-        private void MediaCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
-        {
-            switch(e.Action)
-            {
-                case NotifyCollectionChangedAction.Add:
-                    if (e.NewItems == null)
-                        throw new ArgumentException("Adding items to MediaCollection, but NewItems is null");
-
-                    MediaCollectionChanged_Add(e.NewItems);
-                    break;
-
-                case NotifyCollectionChangedAction.Remove:
-                    if (e.OldItems == null)
-                        throw new ArgumentException("Removing items from MediaCollection, but OldItems is null");
-
-                    MediaCollectionChanged_Remove(e.OldItems);
-                    break;
-
-                case NotifyCollectionChangedAction.Replace:
-                    if (e.NewItems == null || e.OldItems == null)
-                        throw new ArgumentException("Replacing items in MediaCollection, but NewItems or OldItems is null");
-
-                    MediaCollectionChanged_Add(e.NewItems);
-                    MediaCollectionChanged_Remove(e.OldItems);
-                    break;
-
-                case NotifyCollectionChangedAction.Reset:
-                    ResetList();
-                    break;
-            }
-        }
-
-        private void MediaCollectionChanged_Add(IList newItems)
-        {
-            foreach(ICollectable c in newItems)
-            {
-                if (c is Media)
-                    continue;
-
-                // For each new event, add an entry into its proper folder
-                Event e = (Event)c;
-                FolderLabelViewModel month = GetMonthFolder(e.Collection.StartTimestamp);
-                FolderLabelViewModel eventFolder = CreateEventFolder(e);
-                eventFolder.FolderOpened += FolderOpened;
-                month.Children.Add(eventFolder);
-            }
-        }
-
-        private void MediaCollectionChanged_Remove(IList oldItems)
-        {
-            foreach(ICollectable c in oldItems)
-            {
-                PrecisionDateTime ts;
-                if (c is Media)
-                    ts = ((Media)c).Timestamp;
-                else
-                    ts = ((Event)c).Collection.StartTimestamp;
-
-                // Find the year and month folder of the event
-
-                int yearInd = GetYearIndex(ts);
-                if (yearInd == -1)
-                    throw new Exception("Year folder doesn't exist when removing item");
-                FolderLabelViewModel year = Labels[yearInd];
-
-                int monthInd = GetMonthIndex(year, ts);
-                if (monthInd == -1)
-                    throw new Exception("Month folder doesn't exist when removing item");
-                FolderLabelViewModel month = year.Children[monthInd];
-
-
-
-                if (c is Event)
-                {
-                    Event e = (Event)c;
-
-                    // Find the event, and remove it
-                    for (int i = 0; i < month.Children.Count; i++)
-                    {
-                        if (month.Children[i].Label == e.Name)
-                        {
-                            month.Children.RemoveAt(i);
-                            break;
-                        }
-                    }
-                }
-
-                // Find if media exists for the year and month
-                bool monthFound = false;
-                bool yearFound = false;
-                PrecisionDateTime pdt;
-                foreach(ICollectable co in _collection)
-                {
-                    if (co is Media)
-                        pdt = ((Media)co).Timestamp;
-                    else
-                        pdt = ((Event)co).Collection.StartTimestamp;
-
-                    if (pdt.Year == ts.Year)
-                        yearFound = true;
-
-                    if(pdt.Month == ts.Month)
-                    {
-                        monthFound = true;
-                        break;
-                    }
-                }
-
-                // If the month or year folders are now empty, remove them
-                if (!monthFound)
-                    year.Children.RemoveAt(monthInd);
-
-                if (!yearFound)
-                    Labels.RemoveAt(yearInd);
-            }
-        }
-
-        /**
-         * Rebuilds the list of folders. There is a folder for each year and month that contains 
-         * media, and a folder for each event in its month folder.
-         */
-        private void ResetList()
-
-        {
-            _labels.Clear();
-            foreach(ICollectable vm in _collection)
-            {
-                if(vm is Media)
-                {
-                    Media m = (Media)vm;
-                    // Create folder if doesn't exist
-                    FolderLabelViewModel month = GetMonthFolder(m.Timestamp);
-                }
-                else
-                {
-                    Event e = (Event)vm;
-                    // Create folder if doesn't exist
-                    FolderLabelViewModel month = GetMonthFolder(e.Collection.StartTimestamp);
-                    month.Children.Add(CreateEventFolder(e));
-                }
-            }
-        }
-
-
-
-        /** Create a folder for an event, and nest folders for each child event in it */
-        private FolderLabelViewModel CreateEventFolder(Event e)
-        {
-            FolderLabelViewModel vm = new FolderLabelViewModel(e.Name, new PrecisionDateTime(e.Collection.StartTimestamp, TimeRange.Second));
-            vm.FolderOpened += FolderOpened;
-            foreach(ICollectable c in e.Collection)
-            {
-                if (c is Event)
-                    vm.Children.Add(CreateEventFolder((Event)c));
-            }
-            return vm;
-        }
-
-
-
-        /** Gets the index of the given month's folder in the year folder */
-        private int GetMonthIndex(FolderLabelViewModel year, PrecisionDateTime ts)
-        {
-            PrecisionDateTime goal = new PrecisionDateTime(ts, TimeRange.Month);
-
-            int i = 0;
-            for(i = 0; i < year.Children.Count; i++)
-            {
-                if (year.Children[i].Timestamp == goal)
-                    return i;
-                else if (year.Children[i].Timestamp > goal)
-                    break;
-            }
-
-            return -1;
-        }
-
-        /** 
-         * Returns the month folder given by the timestamp. Will create the year or month folder
-         * if they do not already exist
-         */
-        private FolderLabelViewModel GetMonthFolder(PrecisionDateTime ts)
-        {
-            FolderLabelViewModel year = GetYearFolder(ts);
-
-            PrecisionDateTime goal = new PrecisionDateTime(ts, TimeRange.Month);
-
-            int i = 0;
-            for(i = 0; i < year.Children.Count; i++)
-            {
-                if (year.Children[i].Timestamp == goal)
-                    return year.Children[i];
-                else if (year.Children[i].Timestamp > goal)
-                    break;
-            }
-
-            FolderLabelViewModel v = new FolderLabelViewModel(ts.ToString("MMMM"), goal);
-            v.FolderOpened += FolderOpened;
-            year.Children.Insert(i,v);
-            return v;
-        }
-
-        /** Gets the index of the given year's folder in list of folders */
-        private int GetYearIndex(PrecisionDateTime ts)
-        {
-            PrecisionDateTime goal = new PrecisionDateTime(ts, TimeRange.Year);
-
-            int i = 0;
-            for(i = 0; i < Labels.Count; i++)
-            {
-                if (Labels[i].Timestamp == goal)
-                    return i;
-                else if (Labels[i].Timestamp > goal)
-                    break;
-            }
-
-            return -1;
-        }
-
-        /** 
-         * Returns the year folder given by the timestamp. Will create the year folder
-         * if it does not already exist
-         */
-        private FolderLabelViewModel GetYearFolder(PrecisionDateTime ts)
-        {
-            PrecisionDateTime goal = new PrecisionDateTime(ts, TimeRange.Year);
-
-            int i = 0;
-            for(i = 0; i < Labels.Count; i++)
-            {
-                if (Labels[i].Timestamp == goal)
-                    return Labels[i];
-                else if (Labels[i].Timestamp > goal)
-                    break;
-            }
-            FolderLabelViewModel v = new FolderLabelViewModel(ts.Year.ToString(), goal);
-            v.FolderOpened += FolderOpened;
-            Labels.Insert(i,v);
-            return v;
-        }
+        public ObservableCollection<FolderLabelViewModel> Folders
+        { get { return _folders.Folders; } }
 
 
 
@@ -338,13 +93,13 @@ namespace PhotoGalleryApp.ViewModels
             }
         }
 
+
         private RelayCommand _openGalleryCommand;
         public ICommand OpenGalleryCommand => _openGalleryCommand;
 
         public void OpenGallery()
         {
             GalleryViewModel vm = new GalleryViewModel("All Items", _nav, ((MainWindow)System.Windows.Application.Current.MainWindow).Gallery.Collection);
-            //GalleryViewModel vm = new GalleryViewModel("All Items", _nav, new MediaCollection());
             _nav.NewPage(vm);
         }
     }
